9e5906295465e620c4f750e329ebd1d38b4998eb
Vijay <vijay97bk@gmail.com>
2020-12-11 14:00:17 +0530

    part time added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+##!/bin/bash -x
 #employee
 attendance=$((RANDOM%2))
 if [ $attendance -eq 0 ]
@@ -7,3 +7,14 @@ echo employee absent
 else
 echo employee present
 fi
+
+#daily wage
+wph=20
+fdh=8
+daily_wage=$((wph*fdh))
+echo daily wage is $daily_wage
+
+#part time
+parttime=8
+echo part_time_wage is 160
+
