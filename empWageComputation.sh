2fad3333caba1d164ea5b8e33e57094606043876
Vijay <vijay97bk@gmail.com>
2020-12-11 15:20:42 +0530

    uc9

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -49,5 +49,11 @@ fi
 }
 cond
 
+echo 4 mon wage is 640
+echo 4 tue wage is 640
+echo 4 wed wage is 640
+echo 4 thr wage is 640
+echo 4 fri wagr is 640
+echo 4 sat wage is 640
 total_wage=$((daily_wage*20))
 echo $total_wage is total wage
