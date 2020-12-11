331b71a1765ed5cd854fb392e3d2a00fd0afa959
Vijay <vijay97bk@gmail.com>
2020-12-11 14:27:41 +0530

    wage per month after condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -18,3 +18,16 @@ echo daily wage is $daily_wage
 parttime=8
 echo part_time_wage is 160
 
+#wages for month
+working_dpm=20
+wage_month=$((working_dpm*wph*fdh))
+echo wage per month is $wage_month
+
+#calculate wages till a condition of days is reached for a month
+month=20
+if [ $((fdh*month)) -eq 100 ]
+then
+wage_month_condition=($fdh * 100)
+else wage_month_condition=$((fdh*wph*month))
+echo wage per month after condition is $wage_month_condition
+fi
