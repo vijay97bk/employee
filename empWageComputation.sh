d21fbaa5244e54fa951e4c3c6f2437310d3e8309
Vijay <vijay97bk@gmail.com>
2020-12-11 15:10:16 +0530

    uc7

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -x
+##!/bin/bash -x
 #employee
+atte () {
 attendance=$((RANDOM%2))
 if [ $attendance -eq 0 ]
 then
@@ -7,23 +8,37 @@ echo employee absent
 else
 echo employee present
 fi
+}
+atte
 
 #daily wage
+dly_w () {
 wph=20
 fdh=8
 daily_wage=$((wph*fdh))
 echo daily wage is $daily_wage
+}
+dly_w
 
 #part time
+pt () {
 parttime=8
 echo part_time_wage is 160
+}
+pt
 
 #wages for month
+wg_m () {
+dly_w
 working_dpm=20
 wage_month=$((working_dpm*wph*fdh))
 echo wage per month is $wage_month
+}
+wg_m
 
 #calculate wages till a condition of days is reached for a month
+cond () {
+dly_w
 month=20
 if [ $((fdh*month)) -eq 100 ]
 then
@@ -31,3 +46,5 @@ wage_month_condition=($fdh * 100)
 else wage_month_condition=$((fdh*wph*month))
 echo wage per month after condition is $wage_month_condition
 fi
+}
+cond
